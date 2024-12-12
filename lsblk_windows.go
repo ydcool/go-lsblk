79a2858d07cf8b7d4732b9d76be29bddaea0c26b
Dominic Yin <yindongchao@inspur.com>
2024-12-12 19:05:36 +0800

    add windows placeholder

---
+++ lsblk_windows.go
@@ -0,0 +1,13 @@
+package go_lsblk
+
+import (
+	"github.com/pkg/errors"
+)
+
+func ListBlockDevice() ([]BlockDeviceInfo, error) {
+	return nil, errors.New("not yet implemented")
+}
+
+func ListBlockDeviceAsTree() ([]BlockDeviceInfo, error) {
+	return nil, errors.New("not yet implemented")
+}
